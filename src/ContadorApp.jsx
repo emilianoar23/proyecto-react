043b0072839991aec3381e0e75a1bb4f8e24d3a3
emilianoar23 <emilianoar23@gmail.com>
2023-10-28 20:59:49 -0300

    paracticando react

---
+++ src/ContadorApp.jsx
@@ -0,0 +1,10 @@
+export const ContadorApp = () => {
+    return (
+        <>
+            <h1>Contador: </h1>
+            <button onClick={ function (event) {console.log(event)}}>
+                Este es un boton
+            </button>
+        </>
+    )
+}
--- src/PrimerComponente.jsx
+++
@@ -1,8 +0,0 @@
-
-export const PrimerComponente = () => {
-    return (
-        <>
-            <h1>PrimerComponente</h1>
-        </>
-    )
-}
